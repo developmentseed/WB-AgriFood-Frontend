99140a6c14dda677b6d905c5d0de92102cb82319
Vitor George <vitor.george@gmail.com>
2024-02-05 18:29:59 +0000

    Add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import { Box, Heading, Text, Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+import {
+  Box,
+  Heading,
+  Text,
+  Input,
+  Button,
+  FormControl,
+} from "@chakra-ui/react";
 
 function App() {
+  const [formQuestion, setFormQuestion] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert(`Your question has been submitted: ${formQuestion}`);
+  };
+
+  const handleQuestionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormQuestion(e.target.value);
+  };
+
   return (
     <Box maxW="700px" mx="auto" px="4" my="100">
       <Box
@@ -19,7 +39,24 @@ function App() {
         datasets, use cases, and applications related to agriculture and food.
         What specific information are you looking for today?
       </Text>
-      <Input placeholder="Type your question" size="lg" />
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <Input
+            placeholder="Type your question"
+            size="lg"
+            onChange={handleQuestionChange}
+          />
+          <Button
+            type="submit"
+            colorScheme="blue"
+            size="lg"
+            mt={5}
+            isDisabled={formQuestion === ""}
+          >
+            Send Question
+          </Button>
+        </FormControl>
+      </form>
     </Box>
   );
 }
