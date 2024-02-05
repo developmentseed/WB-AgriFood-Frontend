3e0408390222b754b0ffe6dbecdae3c00a4180a1
Vitor George <vitor.george@gmail.com>
2024-02-05 16:53:15 +0000

    Add some components and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Box, Heading, Text, Input } from "@chakra-ui/react";
+
 function App() {
   return (
-    <>
-      <div>Welcome to Agrifood!</div>
-    </>
+    <Box maxW="700px" mx="auto" px="4" my="100">
+      <Box
+        bg="gray.200"
+        w="300px"
+        h="64px"
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        WB Logo
+      </Box>
+      <Heading py={5}>World Bank Agrifood Data Lab</Heading>
+      <Text py={5}>
+        A tool to strengthen data use capacities across the World Bank on
+        datasets, use cases, and applications related to agriculture and food.
+        What specific information are you looking for today?
+      </Text>
+      <Input placeholder="Type your question" size="lg" />
+    </Box>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {};
+
+const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
