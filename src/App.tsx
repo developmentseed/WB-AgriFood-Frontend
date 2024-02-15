d97921f00932cbee6ed6ce2e83a108f8b01ff551
Vitor George <vitor.george@gmail.com>
2024-02-15 16:30:21 +0000

    Remove unused page header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Box, Divider } from '@chakra-ui/react';
+import { Box, Divider } from "@chakra-ui/react";
 
-import Sidebar from './components/Sidebar';
-import Layout from './components/Layout';
-import PageHeader from './components/PageHeader';
-import LandingState from './components/LandingState';
-import QueryForm from './components/QueryForm';
-import Conversation from './components/Conversation';
+import Sidebar from "./components/Sidebar";
+import Layout from "./components/Layout";
+import LandingState from "./components/LandingState";
+import QueryForm from "./components/QueryForm";
+import Conversation from "./components/Conversation";
 
 function App() {
-  const [formQuestion, setFormQuestion] = useState<string>('');
+  const [formQuestion, setFormQuestion] = useState<string>("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -23,7 +22,6 @@ function App() {
 
   return (
     <Layout>
-      {/* <PageHeader /> */}
       <Sidebar />
       <Box
         as="main"
@@ -36,7 +34,16 @@ function App() {
         overflow="hidden"
       >
         {formQuestion.length > 0 ? <Conversation /> : <LandingState />}
-        <Box mt="auto" position="sticky" bottom="0" bg="gray.100" m='-4' p='4' mb='0' pb='0'>
+        <Box
+          mt="auto"
+          position="sticky"
+          bottom="0"
+          bg="gray.100"
+          m="-4"
+          p="4"
+          mb="0"
+          pb="0"
+        >
           <Divider mb="4" borderColor="gray.400" />
           <QueryForm
             formQuestion={formQuestion}
