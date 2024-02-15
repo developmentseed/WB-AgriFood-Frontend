3fdf348cd377d7d72b5e10843bb9b59317bad716
Vitor George <vitor.george@gmail.com>
2024-02-15 14:37:47 +0000

    Add prettier with default settings

--- src/main.tsx
+++ src/main.tsx
@@ -14,5 +14,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
